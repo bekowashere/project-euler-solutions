fe2c9f6ee70b344d626b21f393b9ba871eb3d790
Berke <uberke.karatas@gmail.com>
2023-06-17 05:12:36 +0300

    problem 1 solved

---
+++ euler.py
@@ -0,0 +1,8 @@
+# Project Euler Solutions
+# https://github.com/bekowashere/project-euler-solutions
+
+from typing import Dict
+
+ANSWERS: Dict[int, str] = {
+    1: "233168",
+}
---
+++ p001.py
@@ -0,0 +1,18 @@
+# Solution to Project Euler problem 1
+# https://github.com/bekowashere/project-euler-solutions
+
+"""
+Problem:
+If we list all the natural numbers below 10 that are multiples 3 or 5, we get 3,5,6 and 9.
+The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+
+def compute():
+    answer = [x for x in range(1000) if (x % 3 == 0 or x % 5 == 0)]
+    sum_of_numbers = sum(answer)
+    return str(sum_of_numbers)
+
+
+print(compute())
+print(type(compute()))
